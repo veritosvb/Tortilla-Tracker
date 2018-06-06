7ef1743bfc3a0e56865127d75ae3f1a543a82525
Javier <javier2_90@hotmail.com>
2018-06-05 22:22:02 -0500

    Added ajax pull for the quick search on the start screen. Added ajax query on the results of the search (click an image and get the recipe).

--- assets/javascript/fetch-drink.js
+++ assets/javascript/fetch-drink.js
@@ -1,5 +1,5 @@
 var liquor = "";
-var drinkArr =[];
+var drinkObjectArr =[];
 
 
 $(document).ready(function() {
@@ -26,7 +26,7 @@ $(document).ready(function() {
             console.log(queryURL);
             console.log(response);
             //Saves the drinks matrix on a new variable to go down one level
-            drinkArr = response.drinks;
+            let drinkArr = response.drinks;
             console.log(drinkArr);
             //Variable where the objects will be stored
             drinkObjectArr = [];
@@ -48,18 +48,90 @@ $(document).ready(function() {
             //Goes through the object array and looks for the name and image, then appends them to a div
             for(let j=0; j<drinkArr.length;j++){
                 //Let created inside the for so the append is not overwritten
-                let drinkName = $("<h1>");
+                let drinkName = $("<p>");
                 let drinkImg = $("<img>");
                 //Adding attribute and class to the image
                 drinkImg.attr("src",drinkObjectArr[j].image);
                 drinkImg.addClass("drink-image");
+                drinkImg.addClass("drink-element");
+                drinkImg.attr("drink-id", drinkObjectArr[j].id);
                 //Adding attribute and class to the name
                 drinkName.text(drinkObjectArr[j].name);
                 drinkName.addClass("drink-name");
+                drinkName.addClass("drink-element");
+                drinkName.attr("drink-id", drinkObjectArr[j].id);
                 //Appends to the div
                 $(".drink-images").append(drinkImg);
                 $(".drink-images").append(drinkName);
             }
+        });
+    });
+
+    //Listens for a drink element press
+    $(document).on('click','.drink-element',function(event){
+        console.log(event);
+        console.log(event.target);
+        
+        console.log($(event.target).attr("drink-id"));
+        //Saves the id of the clicked item on a variable
+        let id = $(event.target).attr("drink-id")
+        //Saves the query to a variable
+        var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+id; ;
+        //Calling the ajax for the drink ID
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(response) {
+            //empties the div where the images are stored
+            $('.drink-images').empty();
+            console.log(queryURL);
+            console.log(response);
+            //Saves the relevant data from the pull to a variable
+            let drink = response.drinks[0];
+            console.log(drink);
+            //Adds the container class to structure the div
+            $('.drink-images').addClass("container");
+            //Creates the first row's bootstrap structure
+            let newRow1 = $("<div class='row'>");
+            let newCol1 = $("<div class='col'>");
+            let newCol2 = $("<div class='col'>");
+            newRow1.append(newCol1);
+            newRow1.append(newCol2);
+            
+            //Creates the second row's bootstrap structure
+            let newRow2 = $("<div class='row'>");
+            let newCol3 = $("<div class='col'>");
+            newRow2.append(newCol3);
+            
+            //Creates the div where the ingredients list will be saved
+            let ingredientsDiv = $("<div>");
+            newCol1.append(ingredientsDiv);
+            //Creates the list where the ingredients will be saved
+            let ingredientList = $("<ul>");
+            //Goes through all the ingredients
+            for(let i=1;i<16;i++){
+                //Gets the i'th ingredient name
+                let ingredient = "strIngredient"+i.toString();
+                let measure = "strMeasure"+i.toString();
+                console.log(drink[ingredient]);
+                //New list element to save the ingredient
+                let newli = $("<li>");
+                //If the ingredient is blank it is skipped
+                if(drink[ingredient] !== ""){
+                    //Gets the measure and the ingredient and concatenates them, then adds them to the list element
+                    newli.text(drink[measure] + ": "+drink[ingredient]);
+                    ingredientList.append(newli);
+                    //Gets the instructions and appends it to the corresponding div
+                    $(newCol3).append(drink["strInstructions"]);
+                }
+            }
+            console.log(ingredientList);
+            //Appends the list to the ingredients div
+            ingredientsDiv.append(ingredientList);
+            //Appends the rows to the containers
+            $('.drink-images').append(newRow1);
+            $('.drink-images').append(newRow2);
         });
     });
 });
