109a1990bd388f9de5355d392f99006f2fd8d1a5
marioreg <marioreg@hotmail.com>
2018-06-05 20:47:05 -0500

    contact form info saved to database

--- assets/javascript/authenticate.js
+++ assets/javascript/authenticate.js
@@ -62,3 +62,37 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+$("#form-id").on("submit", function(event){
+    
+    event.preventDefault();
+   
+   var name = $("#input-name").val().trim();
+   var email = $("#input-email").val().trim();
+   var country = $("#select-country").val().trim();
+   var subject = $("#form-subject").val().trim();
+   var message = $("#form-text").val().trim();
+   
+   console.log(name);
+   console.log(email);
+   console.log(country);
+   console.log(subject);
+   console.log(message);
+   
+   database.ref("/contact").push({
+
+       name:name,
+       email:email,
+       country:country,
+       subject:subject,
+       message:message,
+
+   });
+
+   $("#input-name").val("");
+   $("#input-email").val("");
+   $("#select-country").val("");
+   $("#form-subject").val("");
+   $("#form-text").val("");
+
+   });
+   
