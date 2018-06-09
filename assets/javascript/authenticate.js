18a49224266dc039c056b52caff27f633373fc15
Honey B <34516348+elizabethjmz@users.noreply.github.com>
2018-06-09 10:13:27 -0500

    Fixed contact form validation

--- assets/javascript/authenticate.js
+++ assets/javascript/authenticate.js
@@ -62,6 +62,33 @@ function checkRegistrationModal(name,email,cp,pass){
     }
 }
 
+//Function to validate contact form
+function checkContactForm(name,email,subject,message){
+    if (name != "" && email != "" && subject != "" && message != "") {
+    return true;
+    }
+    else {
+    modalAlert("Some required fields are missing!");
+    }
+    }
+//Function to submit contact form after validation and reset fields to its original status after submission
+function sentContactForm(name,email,country,subject,message) {
+    modalAlert("We appreciate you contacting us. One of our colleagues will get back to you shortly. Cheers!");
+    firebase.database().ref("/contact").push({
+    name:name,
+    email:email,
+    country:country,
+    subject:subject,
+    message:message,
+    });
+    $("#error").modal('show');
+    $("#nameC").val("");
+    $("#emailC").val("");
+    //$("#select-country").val("");
+    $("#form-subject").val("");
+    $("#messageC").val(""); 
+}
+    
 //signout account
 function signOut(){
     $("#navSignIn").text("Sign In");
@@ -194,37 +221,17 @@ $("#register").on("click", function(){
 
 
 
+//Contact form when user provides all required fields
 $("#cfsubmit").click(function(event){
-  modalAlert("Thank you for contacting us. We will gate in touch as soon as posible");
-
-   var name = $("#nameC").val().trim();
-   var email = $("#emailC").val().trim();
-   var country = $('#select-country').find(":selected").text();
-   var subject = $("#form-subject").val().trim();
-   var message = $("#messageC").val().trim();
-   
-   console.log(name);
-   console.log(email);
-   console.log(country);
-   console.log(subject);
-   console.log(message);
-   
-   firebase.database().ref("/contact").push({
-       name:name,
-       email:email,
-       country:country,
-       subject:subject,
-       message:message,
-   });
-
-   $("#error").modal('show');
-   $("#input-name").val("");
-   $("#input-email").val("");
-   $("#select-country").val("");
-   $("#form-subject").val("");
-   $("#form-text").val("");
-
-   });
+    var name = $("#nameC").val().trim();
+    var email = $("#emailC").val().trim();
+    var country = $('#select-country').find(":selected").text();
+    var subject = $("#form-subject").val().trim();
+    var message = $("#messageC").val().trim();
+    if(checkContactForm(name,email,subject,message)){
+    sentContactForm(name,email,country,subject,message);
+    }
+    });    
    
 
 function modalAlert(message){
